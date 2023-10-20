7be1ca283d3bdb61fd8fd70a5d6a7c8ec5488bcf
mazemtpl <mazemtpl@gmail.com>
2023-10-20 16:10:18 +0530

    Add Node.js application

--- email-verifier.js
+++
@@ -1,7 +0,0 @@
-const Verifier = require("email-verifier");
- 
-let verifier = new Verifier("your_email_verification_api_key");
-verifier.verify("r@rdegges.com", (err, data) => {
-  if (err) throw err;
-  console.log(data);
-});
